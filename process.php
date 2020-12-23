24453a80a3c0bde56f603587ab1ca6257ce8c8d8
solutionwin <workatwin@gmail.com>
2020-12-23 19:47:06 +0500

    asdasd

--- process.php
+++ process.php
@@ -12,7 +12,7 @@
     ->withDatabaseUri('https://solutionwin.firebaseio.com')
     ->create();
     $database = $firebase->getDatabase();
-    
+    // ss
     function clean($string) {
        $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
        $string = preg_replace('/[^A-Za-z0-9_\-]/', '', $string); // Removes special chars.
